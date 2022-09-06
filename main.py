a76444587fa98bbd3fc5d627ae2271a0faef6a91
Gabriel Duenas <duenasdevelops@gmail.com>
2022-09-06 18:20:07 -0400

    Handle incomplete pitching matchups and drop them

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from bs4 import BeautifulSoup
 import requests
 
 from MlbMatchup import MlbMatchup
+from rankMatchups import dropTbdMatchups, rankMatchups
 
 # load all html of our webpage
-html_text = requests.get('https://www.mlb.com/probable-pitchers').text
+html_text = requests.get('https://www.mlb.com/probable-pitchers/2022-09-07').text
 soup = BeautifulSoup(html_text, 'lxml')
 # container of all pitching matches
 mlb_matchup_container = soup.find('div', class_='container')
@@ -12,6 +13,15 @@ mlb_matchup_container = soup.find('div', class_='container')
 # find current pitching matchup
 mlb_matchup_list = mlb_matchup_container.find_all('div', class_='probable-pitchers__matchup')
 
-for mlb_matchup in mlb_matchup_list:
-    mlb = MlbMatchup(mlb_matchup)
-    print(mlb.getMlbMatchupToString())
+ranked_matchups = []
+
+for matchup in mlb_matchup_list:
+    mlb_matchup = MlbMatchup(matchup)
+    ranked_matchups.append(mlb_matchup)
+
+# drop tbd (incomplete) matchup information
+ranked_matchups = dropTbdMatchups(ranked_matchups)
+
+# oder matchups from most favorable to least favorable
+ranked_matchups = rankMatchups(ranked_matchups)
+
